ef03634163fd2f182a4a7d2c0db3496b8222d884
Ivan <ilitetop@yandex.ru>
2023-06-22 11:42:36 +0700

    init

---
+++ Smart_lift/Smart_lift.cpp
@@ -0,0 +1,10 @@
+﻿#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <openssl/ssl.h>
+#include <boost/thread.hpp>
+#include <iostream>
+
+int main() {
+    std::cout << "HIHIHIH" << std::endl;  
+    
+}
